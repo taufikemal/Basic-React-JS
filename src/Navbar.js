bcdc77f7dbc20568b4ffaf7d9e911cdfc64c31c7
Taufik Kemal Thaha <artcrev7@gmail.com>
2021-02-17 11:49:30 +0700

    Belajar Dasar React JS

---
+++ src/Navbar.js
@@ -0,0 +1,39 @@
+// import React, { Component } from 'react'
+
+// --Class Component---
+
+// export default class Navbar extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h2>Navbar</h2>
+//             </div>
+//         )
+//     }
+// }
+
+
+// --Functuion Component--
+import React from 'react'
+
+// export default function Navbar() {
+//     return (
+//         <div>
+//             <h2>Navbar RCC</h2>
+//         </div>
+//     )
+// }
+
+
+
+
+// --Arrow Component--
+export const Navbar = () => {
+    return (
+        <div>
+            <h2>Navbar RAFC</h2>
+        </div>
+    )
+}
+
+// export default Navbar
