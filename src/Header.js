6652ae0e735f9f3d8ed7d51aaf608bff1f0dffee
Taufik Kemal Thaha <artcrev7@gmail.com>
2021-02-17 11:59:54 +0700

    Edit Header

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <div>
-                <h2>Header</h2>
+                <h2>Header 123</h2>
             </div>
         )
     }
