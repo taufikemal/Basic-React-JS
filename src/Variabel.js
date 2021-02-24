30bf2d8a4998251d161c9ad8a1619cdcc56cbc9e
Taufik Kemal Thaha <artcrev7@gmail.com>
2021-02-24 08:11:29 +0700

    basic variable

---
+++ src/Variabel.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// Const
+// const harga = 3000;
+// harga = 400;
+
+// var
+// var harga = 20000;
+// if(true) {
+//     var harga = 30000;
+// }
+
+// let
+let harga = 20000;
+if(true) {
+    let harga = 30000;
+}
+
+const Variabel = () => {
+    return (
+        <div>
+            <h2>Harga : {harga}</h2>
+        </div>
+    )
+}
+
+export default Variabel
